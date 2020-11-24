0ad8233011d2df29989bfdef5a309bef2b5111c2
Manolis Ragkousis <manolis837@gmail.com>
2020-11-24 20:37:47 +0200

    Add challenge 383 bonus #1 katas

--- clojure-katas/src/clojure_katas/challenge-383.clj
+++ clojure-katas/src/clojure_katas/challenge-383.clj
@@ -8,4 +8,22 @@
   (if (= (count str-a) (count str-b))
       (let [str-b-double (str str-b str-b)]
         (s/includes? str-b-double str-a))
-  false))
+      false))
+
+;; bonus #1
+(defn transpose-and-check-if-same [input-str modified-str times]
+  (if-not (zero? times)
+    (let [head (first modified-str)
+          rest (next modified-str)
+          new-modified-str (str (s/join rest) head)
+          new-times (- times 1)]
+      (if (= input-str modified-str)
+        (+ 1 (transpose-and-check-if-same input-str new-modified-str new-times))
+        (+ 0 (transpose-and-check-if-same input-str new-modified-str new-times))))
+    0))
+
+(defn repeats [input-str]
+  (let [length (count input-str)
+        head (first input-str)
+        rest (next input-str)]
+    (transpose-and-check-if-same input-str (str (s/join rest) head) length)))
